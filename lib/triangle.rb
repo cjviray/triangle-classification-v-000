aaf814430d7839ca09a26bda6be766b1f91044f3
Christian V <viray.christianj@gmail.com>
2019-07-30 00:57:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,6 +21,7 @@ class Triangle
         raise TriangleError
           puts error.message
         end 
+      else if 
       
      
     if (sides[0] >= sides[1] + sides[2]) || (sides[1] >= sides[0] + sides[2]) || (sides[2] >= sides[0] + sides[1])
