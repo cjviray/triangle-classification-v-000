ed426a6c4166b78f3a988c895633802cf9b9b150
Christian V <viray.christianj@gmail.com>
2019-07-30 00:20:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+
+
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene        
+  end 
+  
+  def kind()
+    
+    
+  end 
+
+  class Triangle Error > StandardError
+  end 
 end
